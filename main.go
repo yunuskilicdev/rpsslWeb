75e69176e55441535beb877d604f613f14f1613e
ykdev <yunskilic@gmail.com>
2021-05-28 13:20:09 +0300

    options handler

--- main.go
+++ main.go
@@ -13,5 +13,13 @@ func main() {
 	}
 	FillChoices()
 	router := NewRouter()
+	router.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Printf("OPTIONS")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	})
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
